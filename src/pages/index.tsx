51ca74467fbcb01bbc1257a72c36d5ea3fc7f61e
Dawid Pindara <dawidpa123@gmail.com>
2023-01-06 18:36:39 +0100

    feat: add responsive navigation
    A menu has been added that changes its size depending on the size of the screen.
    Parameters have been added to improve the placement of some elements such as text or buttons.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,12 +49,22 @@ const Space = dynamic(
   }
 );
 
+const MenuDrawer = dynamic(
+  () =>
+    import("@/components/menu").then(
+      ({ MenuDrawer }) => MenuDrawer
+    ),
+  {
+    ssr: false,
+  }
+);
+
 const Home = () => {
   const [mainRef] = useAutoAnimate<HTMLElement>();
 
   const [dateDrawerOpen, setDateDrawerOpen] = useState(false);
   const [satellitesDrawerOpen, setSatellitesDrawerOpen] = useState(false);
-
+  const [menuDrawerOpen, setMenuDrawerOpen] = useState(false);
   return (
     <>
       <Head>
@@ -71,28 +81,21 @@ const Home = () => {
             <path d="M13.229 0A13.229 13.229 0 0 0 0 13.229a13.229 13.229 0 0 0 13.229 13.229 13.229 13.229 0 0 0 1.323-.075A13.229 13.229 0 0 1 2.646 13.23 13.229 13.229 0 0 1 14.59.07 13.229 13.229 0 0 0 13.229 0zM17.237 6.615a2.646 2.646 0 0 0-2.646 2.645v2.646a2.646 2.646 0 0 0 2.646 2.646h7.938v2.646h-7.938a2.646 2.646 0 0 0-2.646 2.646h10.583a2.646 2.646 0 0 0 2.646-2.646v-2.646a2.646 2.646 0 0 0-2.646-2.646h-7.937V9.26h7.937a2.646 2.646 0 0 0 2.646-2.645zM30.466 6.615a2.646 2.646 0 0 0 2.646 2.645h7.937v2.646h-7.937a2.646 2.646 0 0 0-2.646 2.646v5.292h2.646v-5.292h7.937a2.646 2.646 0 0 0 2.646-2.646V9.26a2.646 2.646 0 0 0-2.646-2.645zM52.356 6.615l-7.638 13.229h3.055l4.583-7.938 4.583 7.938h3.055zM64.862 6.615a2.646 2.646 0 0 0-2.646 2.645v7.938a2.646 2.646 0 0 0 2.646 2.646h10.583a2.646 2.646 0 0 0-2.646-2.646h-7.937V9.26h7.937a2.646 2.646 0 0 0 2.646-2.645zM78.091 6.615v13.229H91.32a2.646 2.646 0 0 0-2.646-2.646h-7.937v-2.646h7.937v-2.646h-7.937V9.26h7.937a2.646 2.646 0 0 0 2.646-2.645zM93.966 6.615V9.26h5.292v10.583h2.646V9.26h5.291V6.615zM115.86 6.615l-7.638 13.229h3.055l4.583-7.938 4.583 7.938h3.055zM128.36 6.615a2.646 2.646 0 0 0-2.646 2.645v7.938a2.646 2.646 0 0 0 2.646 2.646h7.938a2.646 2.646 0 0 0 2.645-2.646v-5.292h-7.937v2.646h5.292v2.646h-7.938V9.26h7.938a2.646 2.646 0 0 0 2.645-2.645h-2.645z" />
           </svg>
         </h1>
-        {!dateDrawerOpen && !satellitesDrawerOpen && (
+        {!menuDrawerOpen && !satellitesDrawerOpen && !dateDrawerOpen && (
           <Button
             className="ml-auto"
-            icon={<ManageHistoryIcon className="h-5 w-5 fill-current" />}
-            onClick={() => setDateDrawerOpen(true)}
-            variant="secondary"
-          >
-            Set a date
-          </Button>
-        )}
-        {!dateDrawerOpen && !satellitesDrawerOpen && (
-          <Button
             icon={<MdFormatListBulleted className="h-5 w-5" />}
-            onClick={() => {
-              setSatellitesDrawerOpen(true);
+            onClick={() => {setMenuDrawerOpen(true)
+              setDateDrawerOpen(false);
+              setSatellitesDrawerOpen(false);
             }}
             variant="secondary"
           >
-            Manage satellites
+            Menu
           </Button>
         )}
-        {(dateDrawerOpen || satellitesDrawerOpen) && (
+        
+        {(dateDrawerOpen || satellitesDrawerOpen || menuDrawerOpen) && (
           <Button
             className={clsx(
               (dateDrawerOpen || satellitesDrawerOpen) && "ml-auto"
@@ -101,6 +104,7 @@ const Home = () => {
             onClick={() => {
               setDateDrawerOpen(false);
               setSatellitesDrawerOpen(false);
+              setMenuDrawerOpen(false);
             }}
             variant="secondary"
           >
@@ -122,6 +126,8 @@ const Home = () => {
       </main>
       <DateDrawer open={dateDrawerOpen} />
       <SatellitesDrawer open={satellitesDrawerOpen} />
+      <MenuDrawer open={menuDrawerOpen}  setSatellitesDrawerOpen = {setSatellitesDrawerOpen}  
+      setMenuDrawerOpen = {setMenuDrawerOpen} setDateDrawerOpen = {setDateDrawerOpen}/>
     </>
   );
 };
