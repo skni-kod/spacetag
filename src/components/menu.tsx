f59b6b1146d2fb2b31c1f18c9c3d99eea5c0daaa
Dawid Pindara <80126369+Noname04@users.noreply.github.com>
2023-01-13 15:51:54 +0100

    feat: add responsive navigation (#8)
    Co-authored-by: Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>

---
+++ src/components/menu.tsx
@@ -0,0 +1,46 @@
+import { MdAdd } from "react-icons/md";
+
+import { Button } from "@/components/button";
+import { Drawer } from "@/components/drawer";
+import { ManageHistoryIcon } from "@/components/icons/manage-history";
+
+export type MenuDrawer = {
+  open?: boolean;
+  setDateDrawerOpen: (value: boolean) => void;
+  setMenuDrawerOpen: (value: boolean) => void;
+  setSatellitesDrawerOpen: (value: boolean) => void;
+};
+
+export const MenuDrawer = ({
+  open,
+  setDateDrawerOpen,
+  setMenuDrawerOpen,
+  setSatellitesDrawerOpen,
+}: MenuDrawer) => {
+  return (
+    <Drawer open={open}>
+      <div className="flex flex-col justify-center gap-8 py-4 pb-32 ">
+        <Button
+          className="mx-auto w-3/5"
+          icon={<MdAdd />}
+          onClick={() => {
+            setSatellitesDrawerOpen(true);
+            setMenuDrawerOpen(false);
+          }}
+        >
+          Satellites
+        </Button>
+        <Button
+          className="mx-auto w-3/5"
+          icon={<ManageHistoryIcon className="h-5 w-5 fill-current" />}
+          onClick={() => {
+            setDateDrawerOpen(true);
+            setMenuDrawerOpen(false);
+          }}
+        >
+          Set Date
+        </Button>
+      </div>
+    </Drawer>
+  );
+};
