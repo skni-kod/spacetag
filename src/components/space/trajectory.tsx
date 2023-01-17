293b3388252f6997ea594c81c9cf972a33a262ba
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2023-01-17 18:25:34 +0100

    fix: update content security policy header (#10)

--- src/components/space/trajectory.tsx
+++ src/components/space/trajectory.tsx
@@ -1,9 +1,10 @@
 import { useMemo, useRef, useState } from "react";
 
-import { Text } from "@react-three/drei";
 import { Color, useFrame, useThree } from "@react-three/fiber";
 import { Mesh } from "three";
 import { getSatelliteName } from "tle.js";
+
+import { Text } from "@/components/space/text";
 
 import { useTime } from "@/hooks/time";
 
