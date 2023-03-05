19869d2f437aa54fdb2bdf8c45834052c9340a13
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2023-03-06 00:53:16 +0100

    fix: update content security policy header

--- next.config.mjs
+++ next.config.mjs
@@ -26,10 +26,10 @@ const nextConfig = {
           key: "Content-Security-Policy",
           value: `
           child-src 'none';
-          connect-src 'self' ws: https://fonts.gstatic.com/s/titilliumweb/v15/NaPecZTIAOhVxoMyOr9n_E7fRMc.woff;
+          connect-src 'self' blob: ws: *.gstatic.com;
           default-src 'self';
           frame-ancestors 'none';
-          img-src 'self' data: https://i.imgur.com/jOjPBNg.jpg https://i.imgur.com/0UzB7N1.jpg https://i.imgur.com/Zadamhe.jpg https://i.imgur.com/7nVHtOd.png https://i.imgur.com/l5kIze3.png;
+          img-src 'self' data: https://i.imgur.com/0UzB7N1.jpg https://i.imgur.com/7nVHtOd.png https://i.imgur.com/jOjPBNg.jpg https://i.imgur.com/l5kIze3.png https://i.imgur.com/Zadamhe.jpg;
           media-src 'none';
           script-src 'self' 'unsafe-eval' 'unsafe-inline' blob:;
           style-src 'self' 'unsafe-inline';
